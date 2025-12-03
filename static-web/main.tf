5c7ce767cd710ae59c103b5dc3ca3fdfea533ad6
vaibhav Bhuse <vaibhavbhuse42@gmail.com>
2025-12-03 13:34:15 +0530

    Update main.tf

--- static-web/main.tf
+++ static-web/main.tf
@@ -2,16 +2,17 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Key Pair
 resource "aws_key_pair" "terraform" {
   key_name   = "terraform_final"
-  public_key = file("C:/Users/vaibh/Downloads/terraform_final_clean.pub")
-}
+  public_key = file("C:/Users/vaibh/Downloads/terraform_clean.pub")
 }
 
+# Security Group
 resource "aws_security_group" "blog-web" {
-  name = "blog-web"
-
- 
+  name        = "blog-web"
+  description = "Allow SSH, HTTP, and 8080"
+  
   ingress {
     from_port   = 22
     to_port     = 22
@@ -19,14 +20,12 @@ resource "aws_security_group" "blog-web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 
   ingress {
     from_port   = 8080
@@ -47,16 +46,16 @@ resource "aws_security_group" "blog-web" {
   }
 }
 
-resource "aws_instance" "bolg-web" {
-  ami                    = "ami-02d26659fd82cf299"  
+# EC2 Instance
+resource "aws_instance" "blog-web" {
+  ami                    = "ami-02d26659fd82cf299"  # Amazon Linux 2, ap-south-1
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.blog-web.id]
- key_name               = aws_key_pair.terraform.key_name
+  key_name               = aws_key_pair.terraform.key_name
+
   user_data = file("${path.module}/user_data.sh")
 
   tags = {
     Name = "blog-web"
   }
-  
-  
 }
